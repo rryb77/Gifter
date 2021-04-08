48f4cb9535b574acb7be509582d8c4ae375e4b33
Ryan Youngblood <rryb77@gmail.com>
2021-04-08 11:36:21 -0500

    Added the base of the project with basic CRUD for posts, tested the API

---
+++ Gifter/Repositories/BaseRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace Gifter.Repositories
+{
+    // Setup as an abstract class so it can ONLY be used by inheritance
+    public abstract class BaseRepository
+    {
+        private readonly string _connectionString;
+
+        public BaseRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        // Protected so it is available to child classes, but nowhere else in the code
+        protected SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_connectionString);
+            }
+        }
+    }
+}
