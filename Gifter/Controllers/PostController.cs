ebe2d10eae7afef05227364681394b6a46eec1d8
Ryan Youngblood <rryb77@gmail.com>
2021-04-09 11:27:25 -0500

    Added the ability to search posts by title and caption, also added a hottest endpoint that searches posts via a date given

--- Gifter/Controllers/PostController.cs
+++ Gifter/Controllers/PostController.cs
@@ -71,5 +71,17 @@ namespace Gifter.Controllers
             _postRepository.Delete(id);
             return NoContent();
         }
+
+        [HttpGet("search")]
+        public IActionResult Search(string q, bool sortDesc)
+        {
+            return Ok(_postRepository.Search(q, sortDesc));
+        }
+
+        [HttpGet("hottest")]
+        public IActionResult Hottest(string since, bool sortDesc)
+        {
+            return Ok(_postRepository.Hottest(since, sortDesc));
+        }
     }
 }
